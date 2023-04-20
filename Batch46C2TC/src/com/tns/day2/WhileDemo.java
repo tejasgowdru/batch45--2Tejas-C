35b5557af1db436f1a4f20a7f27e3b5f9b9f6802
Tejas <Tejas@Tejas>
2023-04-20 19:38:54 +0530

    whiledemo

---
+++ Batch46C2TC/src/com/tns/day2/WhileDemo.java
@@ -0,0 +1,16 @@
+package com.tns.day2;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=80;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
